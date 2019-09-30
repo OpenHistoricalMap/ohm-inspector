42c7f777171f4ae6bb7969fac756d2c0a8d3b3e1
Greg Allensworth <gregor@greeninfo.org>
2019-09-30 14:11:36 -0700

    Widget functionality WIP

--- openhistoricalmap-inspector.js
+++ openhistoricalmap-inspector.js
@@ -7,6 +7,7 @@ export class OpenHistoricaMapInspector {
             apiBaseUrl: "https://openhistoricalmap.org/api/",                   // the API URL and version to use
             apiVersion: "0.6",
             classicDivQuerySelector: '#sidebar_content div.browse-section',     // querySelector path to the "classic" inspector output, so we can interact with it, e.g. show/hide
+            featureTitleBar: '#sidebar_content > h2',                               // querySelector path to the title area of the inspector, which is not part of the inspector's readout panel
             onFeatureLoaded: function () {},                                    // give the caller more power, by passing them a copy of features that we load
             debug: false,                                                       // debugging output, mostly useful to developers of this utility
         }, options);
@@ -17,36 +18,63 @@ export class OpenHistoricaMapInspector {
 
         // step 3: create our own DIV in the sidebar, then hide the classic panel
         // we won't actually have anything to show until selectFeature() is called
+        this.oldpanel = document.querySelector(this.options.classicDivQuerySelector);
+        this.titlebar = document.querySelector(this.options.featureTitleBar);
+
         this.mypanel = document.createElement('DIV');
         this.mypanel.classList.add('openhistoricalmap-inspector-panel');
+
+        this.oldpanel.parentNode.insertBefore(this.mypanel, this.oldpanel);
+
         this.hideClassicPanel();
     }
 
     selectFeature(type, id) {
-        //GDA
-        // url = baseurl/version/type/id  e.g.   https://openhistoricalmap.org/api/0.6/way/198180481
+        // construct API URL e.g.   https://openhistoricalmap.org/api/0.6/way/198180481
+        const url = `${this.options.apiBaseUrl}/${this.options.apiVersion}/${type}/${id}`;
+        if (this.options.debug) console.debug(`OpenHistoricaMapInspector selectFeature(${type}, ${id}) => ${url}`);
 
-        //GDA
-        // fetch XML via XHR
+       const request = new XMLHttpRequest();
+        request.open('GET', url);
+        request.onload = () => {
+            // clear existing content
+            this.mypanel.innerHTML = "";
 
-        //GDA
-        // display the output; not really well defined as to needs here, just a few made-up examples of 3-4 fields such as Wikipedia and photos
-        // maybe a few known ones to hardcode here at the top?
+            // handle Not Found
+            if (request.status == 404) {
+                this.titlebar.innerHTML = 'Not Found';
+                this.mypanel.innerHTML = `<p>No such feature: ${type} ${id}</p>`;
+                return;
+            }
+
+            const xmldoc = new DOMParser().parseFromString(request.response, "text/xml");
+            this.renderFeatureDetails(type, id, xmldoc);
+        };
+        request.onerror = function () {
+            alert('Unable to contact the OHM server at this time. Please try again later.');
+        };
+        request.send();
 
         //GDA
         // don't forget to call onFeatureLoaded() so the caller can also get a copy of the parsed feature and maybe do something about it
         // note that the API output only includes propertes and not geometry info, so "simple" stuff like centering on the feature aren't readily feasible
     }
 
+    renderFeatureDetails(type, id, xmldoc) {
+//GDA
+    }
+
     showClassicPanel () {
-        //GDA
         // the pre-existinginspector output is in document.querySelector(classicDivQuerySelector)
         // goal here is to hide our own DIV and show that one
+        this.oldpanel.style.display = 'block';
+        this.mypanel.style.display = 'none';
     }
 
     hideClassicPanel () {
-        //GDA
         // the pre-existinginspector output is in document.querySelector(classicDivQuerySelector)
         // goal here is to hide that DIV and show our own
+        this.oldpanel.style.display = 'none';
+        this.mypanel.style.display = 'block';
     }
 }
